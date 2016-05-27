7bf5c3c3a81fb27137966d478b4d5d8c872f3cdc
Hisaharu Ishii <hisaharu@gmail.com>
2016-05-27 16:58:45 +0900

    init commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -C- "http://fonts.gstatic.com/ea/notosansjapanese/v6/download.zip" -o noto.zip
+unzip -f noto.zip
+convert -size 128x128 xc:transparent -font NotoSansJP-Bold.otf -pointsize 64 -fill teal -annotate +0+56 'せや' -annotate +0+122 'な。' seyana.png
+
